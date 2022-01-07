aaad3a592aff964721ba2e7bb13b5535f73f84e8
Mortis66666 <mortisloverbs@gmail.com>
2022-01-07 11:26:50 +0800

    BOOM

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function (res,req) {
-    res.send("Hi")
+    res.render("index")
 })
 
 app.listen(process.env.PORT || 5000)
