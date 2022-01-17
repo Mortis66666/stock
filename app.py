62a374e0e0c715d745a79c28bbb690dc8ce61c11
Mortis66666 <mortisloverbs@gmail.com>
2022-01-18 00:02:03 +0800

    Added login and signup route

--- app.py
+++ app.py
@@ -8,7 +8,16 @@ def index():
     
     return render_template("index.html")
 
+@app.route('/login')
+def login():
 
+    return render_template("login.html")
+
+
+@app.route('/signup')
+def signup():
+
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run()
