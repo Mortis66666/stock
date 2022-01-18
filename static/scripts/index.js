b150c4d26e27cd087c5937a5663c2a9f1ed7a320
Mortis66666 <mortisloverbs@gmail.com>
2022-01-18 15:14:13 +0800

    button mouseenter styling

---
+++ static/scripts/index.js
@@ -0,0 +1,15 @@
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("mouseenter",(event)=>{
+    loginButton.style.color = "gold";
+})
+loginButton.addEventListener("mouseleave",(event)=>{
+    loginButton.style.color = "black";
+})
+
+const loginButton = document.getElementById("signUpButton");
+signUpButton.addEventListener("mouseenter",(event)=>{
+    loginButton.style.color = "gold";
+})
+signUpButton.addEventListener("mouseleave",(event)=>{
+    loginButton.style.color = "black";
+})
