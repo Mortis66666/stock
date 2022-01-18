d21fbffe42db1ad71ea986b6d71e17f93a1d355e
Mortis66666 <mortisloverbs@gmail.com>
2022-01-18 23:39:06 +0800

    Login useless form

---
+++ static/scripts/login.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById("submit");
+
+submit.addEventListener("mouseenter",(event)=>{
+    submit.style.color = "gold";
+})
+
+submit.addEventListener("mouseleave",(event)=>{
+    submit.style.color = "#00FF73";
+})
