c182fae1ee05944deb1a0dd4aec765d16c47cfe3
Mortis66666 <mortisloverbs@gmail.com>
2022-01-19 15:09:54 +0800

    Added some messy code (Not sure it will work or not

---
+++ static/scripts/signup.js
@@ -0,0 +1,50 @@
+function makeRed (object) {
+    object.style.borderColor = "red";
+}
+
+function addWarning (id, warning) {
+    let element = document.getElementById(id);
+    element.innerHTML = warning;
+}
+
+function removeWarning (id) {
+    let element = document.getElementById(id);
+    element.innerHTML = "";
+}
+
+function warn (object, warningId, reason) {
+    makeRed(object);
+    addWarning(warningId,reason)
+}
+
+function pass(object,warningId) {
+    object.style.borderColor = "green";
+    removeWarning(warningId);
+}
+
+
+function onInput (event) {
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    let cpassword = document.getElementById("cpassword");
+
+    // Username warning
+    const uname = username.value;
+    var id = "userWarning";
+
+    if (uname.length < 4) {
+        warn(username,id,"Username must have at least 4 characters");
+    }
+    else if (uname.length > 15) {
+        warn(username,id,"Username cannot be more than 15 characters");
+    }
+}
+
+var elements = [];
+elements.push(document.getElementById("username"));
+elements.push(document.getElementById("password"));
+elements.push(document.getElementById("cpassword"));
+
+for (let element in elements) {
+    element.addEventListener("oninput",onInput)
+}
