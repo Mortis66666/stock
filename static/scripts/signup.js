9cc015b60334deeeea6f478f25b3a913276ae1a7
Mortis66666 <mortisloverbs@gmail.com>
2022-01-19 16:02:22 +0800

    pls give me a warning

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -40,4 +40,4 @@ function onInput () {
     }
 }
 
-document.getElementById("signupForm").addEventListener("oninput",onInput);
+document.getElementById("signupForm").addEventListener("oninput",oninput());
