00b04509b98d00672de2a53a3e6cb5614ca3c7ce
Mortis66666 <mortisloverbs@gmail.com>
2022-01-20 14:58:21 +0800

    Error handler 404 page not found

--- app.py
+++ app.py
@@ -19,5 +19,11 @@ def signup():
 
     return render_template("signup.html")
 
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page_not+found.html"), 404
+
 if __name__ == "__main__":
     app.run()
