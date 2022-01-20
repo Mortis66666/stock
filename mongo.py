01d5692867d4a6e35e090d276d2a48c331833191
Mortis66666 <mortisloverbs@gmail.com>
2022-01-20 23:56:47 +0800

    valid_login

--- mongo.py
+++ mongo.py
@@ -1,7 +1,36 @@
 from pymongo import MongoClient
 import os
+from typing import List, Union
 
 
 db_url = os.environ["MONGO_URL"]
 client = MongoClient(db_url)
+
+collection = client.stocks.profile
+
+
+def valid_login(username: str, password: str) -> List[bool,str]:
+
+    results = collection.find_one({
+        "username": username,
+        "password": password
+    })
+
+    if results is None:
+        return False, "Incorrect username or password"
+    else:
+        return True, "Success"
+    
+
+
+def valid_signup(username: str, password: str, confirm_password: str) -> List[bool,str]:
+    pass
+
+
+def get_bal(username: str) -> Union[int,None]:
+    pass
+
+
+def add_bal(username: str) -> None:
+    pass
 
