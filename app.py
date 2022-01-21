3021faaa8b878bf33ad09eeab185f5f6dece8b4e
Mortis66666 <mortisloverbs@gmail.com>
2022-01-21 10:38:13 +0800

    added some changes

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for, session
+from mongo import *
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex()
 
 
+# Routing
 @app.route('/')
 def index():
     
@@ -11,7 +15,7 @@ def index():
 @app.route('/login')
 def login():
 
-    return render_template("login.html")
+    return render_template("login.html", warning="")
 
 
 @app.route('/signup')
@@ -19,10 +23,37 @@ def signup():
 
     return render_template("signup.html")
 
+@app.route('/home')
+def home():
+    if "username" not in session:
+        return redirect(url_for("login"))
+    username = session["username"]
+
+    
 
 
+@app.route('/valid', methods = ["POST", "GET"])
+def valid():
+
+    if request.method == "POST":
+        form = request.form
+        username = form["username"]
+        password = form["password"]
+        valid, message = valid_login(username, password)
+        if valid:
+            session["username"] = username
+            return redirect(url_for('home'))
+        return redirect(url_for('login'))
+
+
+
+
+
+
+# Error handling
 @app.errorhandler(404)
 def page_not_found(error):
+
     return render_template("page_not_found.html"), 404
 
 if __name__ == "__main__":
