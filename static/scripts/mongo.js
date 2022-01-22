ff648eefec04b26903b526747c76ed8c03302f8d
Mortis66666 <mortisloverbs@gmail.com>
2022-01-22 16:06:40 +0800

    test

--- static/scripts/mongo.js
+++ static/scripts/mongo.js
@@ -1,13 +1,22 @@
 const {MongoClient} = require('mongodb');
-const db_url = process.env.MONGO_URL;
 
+const db_url = process.env.MONGO_URL;
 const client = new MongoClient(db_url);
 
 
-async function main () {
+async function get_bal (username) {
     try {
-        const profiles = client.db("stocks").collection("profiles");
+        await client.connect() // Connect to the client
+
+        const profiles = client.db("stocks").collection("profiles"); // Gets collection "profile"
+        var result = profiles.findOne(
+            {
+                username: username
+            }
+        )
     }
+
+
     finally {
         await client.close();
     }
