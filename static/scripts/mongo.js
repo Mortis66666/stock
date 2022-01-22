2a34137865399469497bc0c46134c1f776d92442
Mortis66666 <mortisloverbs@gmail.com>
2022-01-23 00:40:22 +0800

    Added some functions (undone)

--- static/scripts/mongo.js
+++ static/scripts/mongo.js
@@ -1,23 +1,49 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const db_url = process.env.MONGO_URL;
-const client = new MongoClient(db_url);
+const db_url = process.env.MONGO_URL; // Gets the url in env
+const client = new MongoClient(db_url); // Mongoclient
 
 
-async function get_bal (username) {
+async function get_user_info (username) {
     try {
         await client.connect() // Connect to the client
-
-        const profiles = client.db("stocks").collection("profiles"); // Gets collection "profile"
-        var result = profiles.findOne(
+        var profiles = client.db("stocks").collection("profiles"); // Gets collection "profile"
+        var result = await profiles.findOne(
             {
-                username: username
+                "username": username
             }
-        )
+        );
+        return result;
     }
 
+    catch (error) {
+        return null;
+    }
 
     finally {
         await client.close();
+    }
+}
+
+async function add_coins (username, amount) {
+    try {
+        await client.connect()
+        var profiles = client.db("stocks").collection("profiles");
+
+        profiles.updateOne(
+            {
+                "username": username
+            },
+            {
+                "$inc": {
+                    "coins": amount
+                }
+            }
+        )
+
+    }
+
+    finally {
+        await client.close()
     }
 }
