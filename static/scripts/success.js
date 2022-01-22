2eeb562cc3fc545bb19f217742a0034712b97027
Mortis66666 <mortisloverbs@gmail.com>
2022-01-22 10:29:46 +0800

    Success styling

---
+++ static/scripts/success.js
@@ -0,0 +1,9 @@
+// Login Button
+
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("mouseenter",(event)=>{
+    loginButton.style.color = "gold";
+})
+loginButton.addEventListener("mouseleave",(event)=>{
+    loginButton.style.color = "black";
+})
