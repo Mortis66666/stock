2eb569d374ca91d7690f5e757685e8245681ac73
Mortis66666 <mortisloverbs@gmail.com>
2022-02-05 14:23:05 +0800

    .

---
+++ static/scripts/index.js
@@ -0,0 +1,19 @@
+// Login Button
+
+const loginButton = document.getElementById("loginButton");
+loginButton.addEventListener("mouseenter",(event)=>{
+    loginButton.style.color = "gold";
+})
+loginButton.addEventListener("mouseleave",(event)=>{
+    loginButton.style.color = "black";
+})
+
+// Signup Button
+
+const signUpButton = document.getElementById("signUpButton");
+signUpButton.addEventListener("mouseenter",(event)=>{
+    signUpButton.style.color = "gold";
+})
+signUpButton.addEventListener("mouseleave",(event)=>{
+    signUpButton.style.color = "black";
+})
