8ec49d821e2866f1091464286c8981f7500f7046
Mortis66666 <mortisloverbs@gmail.com>
2022-02-05 23:36:26 +0800

    fix everything

--- mongo.py
+++ mongo.py
@@ -244,7 +244,7 @@ def task():
                             20 + stock_left/100,
                             35
                         ]
-                    )
+                    )[0]
 
                 case "dec":
 
