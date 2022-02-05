fe2fd6d05e3ea358795f3f05a03c89059125753e
Mortis66666 <mortisloverbs@gmail.com>
2022-02-05 15:42:09 +0800

    test

---
+++ static/scripts/mystocks.js
@@ -0,0 +1,35 @@
+const message = document.getElementById("message").value;
+
+if (message) {
+    alert(message);
+};
+
+function increase (name) {
+
+    const id = "amount"+name;
+    const element = document.getElementById(id);
+    const price = document.getElementById("price"+name);
+    const amount = parseInt(element.innerText);
+
+    element.innerText = amount + 1;
+    price.innerText = "💰" + ((parseInt(price.innerText.replace("💰", "")) / amount) * (amount + 1));
+};
+
+function decrease (name) {
+
+    const id = "amount"+name;
+    const element = document.getElementById(id);
+    const price = document.getElementById("price"+name);
+    const amount = parseInt(element.innerText);
+
+    if (amount > 1) {
+        element.innerText = amount - 1;
+        price.innerText = "💰" + ((parseInt(price.innerText.replace("💰", "")) / amount) * (amount - 1));
+    }
+};
+
+function buy (stock_owner) {
+
+    const amount = document.getElementById("amount"+stock_owner).innerText;
+    window.location.href = `https://mortis666stocksimulator.herokuapp.com/buy?user=${stock_owner}&amount=${amount}`;
+};
