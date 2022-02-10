b109db92793edfdb2eab4abbcc8acf2f5d7780a3
Mortis66666 <mortisloverbs@gmail.com>
2022-02-10 16:58:29 +0800

    test real time update

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -33,3 +33,32 @@ function buy (stock_owner) {
     const amount = document.getElementById("amount"+stock_owner).innerText;
     window.location.href = `https://mortis666stocksimulator.herokuapp.com/buy?user=${stock_owner}&amount=${amount}&redirect=${window.location.href}`;
 };
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+while (true) {
+    $.getJSON("https://mortis666stocksimulator.herokuapp.com/stock_api", data,
+        function (data, textStatus, jqXHR) {
+            
+            data.forEach(object => {
+                
+                var name = object.username;
+                var price = object.stock_value;
+                var result = getElementById("price"+name);
+                var amount = getElementById("amount"+name);
+
+                if (result) {
+                    if (amount.innerText == 1) {
+                        result.innerText = price;
+                    };
+                };
+
+            });
+
+        }
+    );
+
+    sleep(3000);
+}
